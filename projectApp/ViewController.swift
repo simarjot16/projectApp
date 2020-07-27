d535011313d6db028ab89dac1a7750737f424d77
Simarjot Singh <singh.16.simarjot@gmail.com>
2020-07-27 16:50:25 +0530

    Initial Commit

---
+++ projectApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  projectApp
+//
+//  Created by Simarjot Singh on 27/07/20.
+//  Copyright © 2020 Simarjot Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
